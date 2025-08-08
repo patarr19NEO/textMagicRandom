f7155ae5025e0bbe4d6cc1014e5718e2cee68610
patarr19NEO <yurapatarr19@yandex.ru>
2025-08-08 23:12:08 +0300

    Update test_lib_v1.py
    добавлены функции: тестирование функция для тестирования функция; поиск редкого для поиска редких никнеймов

--- test_lib_v1.py
+++ test_lib_v1.py
@@ -1,15 +1,35 @@
-#how to use lib
+import time
 
 from text_magic_random import *
 
-#генерация ника
-nickname = generator.generate_nicknames()
-print(nickname)
+def searchin_rare():
+    while True:
+        nickname = generator.generate_nicknames()
+        if nickname == "Черный_Раб_42" or nickname == "Розовый_Единорог_42":
+            time.sleep(0.1)
+            print("ОН НАШЕЛСЯ!")
+            break
+        else:
+            time.sleep(0.1)
+            print(nickname)
 
-#генерацию текста-заглушки
-stub_text = generator.text_stub(10)
-print(stub_text)
+def testing_featers():
+    # Никнейм для игры
+    print(generator.generate_nicknames()) # Пример: "Безумный_Скелет_42"
 
-#генерация пароля
-password = generator.generate_password(16)
-print(password)
+    # Надёжный пароль
+    print(generator.generate_password(10)) # Пример: "k3F$mX8!pL"
+
+    # Текст-заглушка
+    print(generator.text_stub(3)) # Пример: "Пубтджи яни лосось"
+
+    print(generator.search_letter("Параллелепипед", "л"))
+
+if __name__ == '__main__':
+    choice = input("Найти редкие - 1\nТестирование функций - 2")
+    if choice == "1":
+        searchin_rare()
+    elif choice == "2":
+        testing_featers()
+    else:
+        print("Инвалид")
