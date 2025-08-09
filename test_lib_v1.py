b7dd5d17af3a933a969e4ad734f917f4ace1d391
patarr19NEO <yurapatarr19@yandex.ru>
2025-08-09 12:57:42 +0300

    Update test_lib_v1.py
    исправлена критическая ошибка в search_rare()

--- test_lib_v1.py
+++ test_lib_v1.py
@@ -5,7 +5,7 @@ from text_magic_random import *
 def searchin_rare():
     while True:
         nickname = generator.generate_nicknames()
-        if nickname == "Черный_Раб_42" or nickname == "Розовый_Единорог_42":
+        if "Черный_Раб_42" or "Розовый_Единорог_42" in nickname:
             time.sleep(0.1)
             print("ОН НАШЕЛСЯ!")
             break
@@ -23,7 +23,11 @@ def testing_featers():
     # Текст-заглушка
     print(generator.text_stub(3)) # Пример: "Пубтджи яни лосось"
 
+    # скока л
     print(generator.search_letter("Параллелепипед", "л"))
+
+    # кодирование
+    print(generator.code("hello")) # превратится в svool
 
 if __name__ == '__main__':
     choice = input("Найти редкие - 1\nТестирование функций - 2")
